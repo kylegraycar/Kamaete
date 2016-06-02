082d6ff73b0f101cf18ea6cb47b693882673d8b6
Joel Graycar <jgraycar@berkeley.edu>
2016-06-01 15:39:21 -1000

    Add user settings route, remove scruff from navbar

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function () {
   this.route('performances');
   this.route('login');
   this.route('register');
+  this.route('account-settings', { path: '/settings' });
 });
 
 export default Router;
