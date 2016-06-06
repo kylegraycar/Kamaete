afa0c6c17f7aadcf19eaabe1404f603855487fbd
Joel Graycar <jgraycar@berkeley.edu>
2016-06-06 02:35:33 -0700

    Add login form to navbar for medium-and-up screens

--- app/pods/components/nav-bar/component.js
+++ app/pods/components/nav-bar/component.js
@@ -3,8 +3,30 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'nav',
   session: Ember.inject.service('session'),
+  flashMessages: Ember.inject.service(),
 
   actions: {
+    authenticate() {
+      const flashMessages = Ember.get(this, 'flashMessages');
+      flashMessages.clearMessages();
+
+      this.get('session').authenticate('authenticator:devise-token',
+          this.get('navbar-email'), this.get('navbar-password')).then(() => {
+        flashMessages.success('Login successful; welcome back!', {
+          timeout: 10000
+        });
+      },
+
+      (reason) => {
+        reason.errors.forEach((error) => {
+          flashMessages.danger(error, {
+            sticky: true
+          });
+          this.sendAction('loginFailed');
+        });
+      });
+    },
+
     invalidateSession() {
       this.get('session').invalidate();
     }
