dc9e9d03047bf9fda6686b603fa6da6847375c5c
Joel Graycar <jgraycar@berkeley.edu>
2016-06-14 17:00:48 -0700

    Add jumbotron to homepage

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  renderTemplate: function() {
+    this.render();
+    this.render('components/welcome-jumbotron', {
+      into: 'application',
+      outlet: 'jumbotron'
+    });
+  }
 });
